e2edf00d8a986c49b6ed93259e042ad4b938677a
Chemicalz82 <chemcope82@yahoo.com>
2019-04-15 18:47:10 -0500

    Added test data

--- Lab_7/MW19_L7_Copeland.py
+++ Lab_7/MW19_L7_Copeland.py
@@ -135,3 +135,512 @@ def menu():
             print("\n=====     Invalid Selection!     =====")
 
 menu()
+
+## Testing
+# hpd15@LAPTOP-P4P9TKIA MINGW64 ~/Desktop/ACC Classes/Spring 2019/fundamentals_of_programming_1/Lab_7 (master)
+# $ python MW19_L7_Copeland.py
+
+# =========================================
+
+# Please choose what you'd like to perform:
+
+# R) Read File
+# W) Write to File
+# I) Insert in File
+# D) Delete from File
+# F) Find in File
+# S) Sort a File
+# V) View Contents of File
+# Q) Quit
+
+# Your choice: r
+
+# =========================================
+
+
+# This option will read a file into a list for you.
+
+# Please enter the file name: peeple
+
+# =====     Error!     =====
+
+# The following error occurred: [Errno 2] No such file or directory: 'peeple.txt'
+
+# =========================================
+
+# Please choose what you'd like to perform:
+
+# R) Read File
+# W) Write to File
+# I) Insert in File
+# D) Delete from File
+# F) Find in File
+# S) Sort a File
+# V) View Contents of File
+# Q) Quit
+
+# Your choice: r
+
+# =========================================
+
+
+# This option will read a file into a list for you.
+
+# Please enter the file name: people.txt
+
+# Your file was successfully read.
+
+# =========================================
+
+# Please choose what you'd like to perform:
+
+# R) Read File
+# W) Write to File
+# I) Insert in File
+# D) Delete from File
+# F) Find in File
+# S) Sort a File
+# V) View Contents of File
+# Q) Quit
+
+# Your choice: v
+
+# =========================================
+
+
+# This is a numbered display of the names currently stored in your list:
+
+
+# 1) Lu, 2) Joe, 3) Luis, 4) Zelda, 5) Chad, 6) Nguyen, 7) Mindy, 8) Anne, 9) Lee, 10) George,
+
+# 11) Max, 12) Antoine, 13) Terry, 14) Maria, 15) An, 16) Victor, 17) Larry, 18) Steve, 19) Anita, 20) Joline,
+
+# 21) Charles,
+# =========================================
+
+# Please choose what you'd like to perform:
+
+# R) Read File
+# W) Write to File
+# I) Insert in File
+# D) Delete from File
+# F) Find in File
+# S) Sort a File
+# V) View Contents of File
+# Q) Quit
+
+# Your choice: s
+
+# Your list has been sorted.
+
+# =========================================
+
+# Please choose what you'd like to perform:
+
+# R) Read File
+# W) Write to File
+# I) Insert in File
+# D) Delete from File
+# F) Find in File
+# S) Sort a File
+# V) View Contents of File
+# Q) Quit
+
+# Your choice: v
+
+# =========================================
+
+
+# This is a numbered display of the names currently stored in your list:
+
+
+# 1) An, 2) Anita, 3) Anne, 4) Antoine, 5) Chad, 6) Charles, 7) George, 8) Joe, 9) Joline, 10) Larry,
+
+# 11) Lee, 12) Lu, 13) Luis, 14) Maria, 15) Max, 16) Mindy, 17) Nguyen, 18) Steve, 19) Terry, 20) Victor,
+
+# 21) Zelda,
+# =========================================
+
+# Please choose what you'd like to perform:
+
+# R) Read File
+# W) Write to File
+# I) Insert in File
+# D) Delete from File
+# F) Find in File
+# S) Sort a File
+# V) View Contents of File
+# Q) Quit
+
+# Your choice: f
+
+# =========================================
+
+
+# This option will allow you to search for a name in the list.
+
+# What name would you like to check for in the list? max
+
+# Max is already in the list.
+
+# =========================================
+
+# Please choose what you'd like to perform:
+
+# R) Read File
+# W) Write to File
+# I) Insert in File
+# D) Delete from File
+# F) Find in File
+# S) Sort a File
+# V) View Contents of File
+# Q) Quit
+
+# Your choice: f
+
+# =========================================
+
+
+# This option will allow you to search for a name in the list.
+
+# What name would you like to check for in the list? john
+
+# john is not in the list.
+
+# =========================================
+
+# Please choose what you'd like to perform:
+
+# R) Read File
+# W) Write to File
+# I) Insert in File
+# D) Delete from File
+# F) Find in File
+# S) Sort a File
+# V) View Contents of File
+# Q) Quit
+
+# Your choice: i
+
+# =========================================
+
+
+# This option will allow you to insert a name into the list.
+
+# What name would you like to insert in the list? john
+
+# At what position number would you like to insert the name? 5
+
+# John has been inserted at position 5
+
+# =========================================
+
+# Please choose what you'd like to perform:
+
+# R) Read File
+# W) Write to File
+# I) Insert in File
+# D) Delete from File
+# F) Find in File
+# S) Sort a File
+# V) View Contents of File
+# Q) Quit
+
+# Your choice: v
+
+# =========================================
+
+
+# This is a numbered display of the names currently stored in your list:
+
+
+# 1) An, 2) Anita, 3) Anne, 4) Antoine, 5) John, 6) Chad, 7) Charles, 8) George, 9) Joe, 10) Joline,
+
+# 11) Larry, 12) Lee, 13) Lu, 14) Luis, 15) Maria, 16) Max, 17) Mindy, 18) Nguyen, 19) Steve, 20) Terry,
+
+# 21) Victor, 22) Zelda,
+# =========================================
+
+# Please choose what you'd like to perform:
+
+# R) Read File
+# W) Write to File
+# I) Insert in File
+# D) Delete from File
+# F) Find in File
+# S) Sort a File
+# V) View Contents of File
+# Q) Quit
+
+# Your choice: i
+
+# =========================================
+
+
+# This option will allow you to insert a name into the list.
+
+# What name would you like to insert in the list? max
+
+# Max is already in the list
+
+# =========================================
+
+# Please choose what you'd like to perform:
+
+# R) Read File
+# W) Write to File
+# I) Insert in File
+# D) Delete from File
+# F) Find in File
+# S) Sort a File
+# V) View Contents of File
+# Q) Quit
+
+# Your choice: i
+
+# =========================================
+
+
+# This option will allow you to insert a name into the list.
+
+# What name would you like to insert in the list? robby
+
+# At what position number would you like to insert the name? 25
+
+# Robby has been inserted at the end of the list.
+
+# =========================================
+
+# Please choose what you'd like to perform:
+
+# R) Read File
+# W) Write to File
+# I) Insert in File
+# D) Delete from File
+# F) Find in File
+# S) Sort a File
+# V) View Contents of File
+# Q) Quit
+
+# Your choice: v
+
+# =========================================
+
+
+# This is a numbered display of the names currently stored in your list:
+
+
+# 1) An, 2) Anita, 3) Anne, 4) Antoine, 5) John, 6) Chad, 7) Charles, 8) George, 9) Joe, 10) Joline,
+
+# 11) Larry, 12) Lee, 13) Lu, 14) Luis, 15) Maria, 16) Max, 17) Mindy, 18) Nguyen, 19) Steve, 20) Terry,
+
+# 21) Victor, 22) Zelda, 23) Robby,
+# =========================================
+
+# Please choose what you'd like to perform:
+
+# R) Read File
+# W) Write to File
+# I) Insert in File
+# D) Delete from File
+# F) Find in File
+# S) Sort a File
+# V) View Contents of File
+# Q) Quit
+
+# Your choice: s
+
+# Your list has been sorted.
+
+# =========================================
+
+# Please choose what you'd like to perform:
+
+# R) Read File
+# W) Write to File
+# I) Insert in File
+# D) Delete from File
+# F) Find in File
+# S) Sort a File
+# V) View Contents of File
+# Q) Quit
+
+# Your choice: v
+
+# =========================================
+
+
+# This is a numbered display of the names currently stored in your list:
+
+
+# 1) An, 2) Anita, 3) Anne, 4) Antoine, 5) Chad, 6) Charles, 7) George, 8) Joe, 9) John, 10) Joline,
+
+# 11) Larry, 12) Lee, 13) Lu, 14) Luis, 15) Maria, 16) Max, 17) Mindy, 18) Nguyen, 19) Robby, 20) Steve,
+
+# 21) Terry, 22) Victor, 23) Zelda,
+# =========================================
+
+# Please choose what you'd like to perform:
+
+# R) Read File
+# W) Write to File
+# I) Insert in File
+# D) Delete from File
+# F) Find in File
+# S) Sort a File
+# V) View Contents of File
+# Q) Quit
+
+# Your choice: d
+
+# =========================================
+
+
+# This option will allow you to delete a name from the list.
+
+# Enter the name you want to delete: mary
+
+# Mary is not in the list.
+
+# =========================================
+
+# Please choose what you'd like to perform:
+
+# R) Read File
+# W) Write to File
+# I) Insert in File
+# D) Delete from File
+# F) Find in File
+# S) Sort a File
+# V) View Contents of File
+# Q) Quit
+
+# Your choice: d
+
+# =========================================
+
+
+# This option will allow you to delete a name from the list.
+
+# Enter the name you want to delete: maria
+
+# Maria was deleted from the list.
+
+# =========================================
+
+# Please choose what you'd like to perform:
+
+# R) Read File
+# W) Write to File
+# I) Insert in File
+# D) Delete from File
+# F) Find in File
+# S) Sort a File
+# V) View Contents of File
+# Q) Quit
+
+# Your choice: v
+
+# =========================================
+
+
+# This is a numbered display of the names currently stored in your list:
+
+
+# 1) An, 2) Anita, 3) Anne, 4) Antoine, 5) Chad, 6) Charles, 7) George, 8) Joe, 9) John, 10) Joline,
+
+# 11) Larry, 12) Lee, 13) Lu, 14) Luis, 15) Max, 16) Mindy, 17) Nguyen, 18) Robby, 19) Steve, 20) Terry,
+
+# 21) Victor, 22) Zelda,
+# =========================================
+
+# Please choose what you'd like to perform:
+
+# R) Read File
+# W) Write to File
+# I) Insert in File
+# D) Delete from File
+# F) Find in File
+# S) Sort a File
+# V) View Contents of File
+# Q) Quit
+
+# Your choice: w
+
+# =========================================
+
+
+# This option will write your current list into a new file for you.
+# You must enter a file name to write to.
+
+# Please enter the file name: people_sorted
+
+# Your file was successfully written.
+
+# =========================================
+
+# Please choose what you'd like to perform:
+
+# R) Read File
+# W) Write to File
+# I) Insert in File
+# D) Delete from File
+# F) Find in File
+# S) Sort a File
+# V) View Contents of File
+# Q) Quit
+
+# Your choice: r
+
+# =========================================
+
+
+# This option will read a file into a list for you.
+
+# Please enter the file name: people_sorted
+
+# Your file was successfully read.
+
+# =========================================
+
+# Please choose what you'd like to perform:
+
+# R) Read File
+# W) Write to File
+# I) Insert in File
+# D) Delete from File
+# F) Find in File
+# S) Sort a File
+# V) View Contents of File
+# Q) Quit
+
+# Your choice: v
+
+# =========================================
+
+
+# This is a numbered display of the names currently stored in your list:
+
+
+# 1) An, 2) Anita, 3) Anne, 4) Antoine, 5) Chad, 6) Charles, 7) George, 8) Joe, 9) John, 10) Joline,
+
+# 11) Larry, 12) Lee, 13) Lu, 14) Luis, 15) Max, 16) Mindy, 17) Nguyen, 18) Robby, 19) Steve, 20) Terry,
+
+# 21) Victor, 22) Zelda,
+# =========================================
+
+# Please choose what you'd like to perform:
+
+# R) Read File
+# W) Write to File
+# I) Insert in File
+# D) Delete from File
+# F) Find in File
+# S) Sort a File
+# V) View Contents of File
+# Q) Quit
+
+# Your choice: q
+
+# hpd15@LAPTOP-P4P9TKIA MINGW64 ~/Desktop/ACC Classes/Spring 2019/fundamentals_of_programming_1/Lab_7 (master)
+# $
